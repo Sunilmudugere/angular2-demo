6c9a7a82c245cfdceecccdab15cd12cf4c26f357
M P Sunil <sunil.mp@tavant.com>
2019-06-22 02:29:32 +0530

    firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FormToolbarComponent }   from './components/form-toolbar.component';
+// import { FormToolbarComponent }   from './components/form-toolbar.component';
 import { HTMLLayoutComponent }   from './components/html-layout.component';
-import { WebixLayoutComponent }   from './components/webix-layout.component';
-import { TemplateRoutingComponent } from './components/template-routing.component';
-import { DataLoadingComponent } from './components/data-loading.component';
+// import { WebixLayoutComponent }   from './components/webix-layout.component';
+// import { TemplateRoutingComponent } from './components/template-routing.component';
+// import { DataLoadingComponent } from './components/data-loading.component';
 
 const routes: Routes = [
-  { path: 'webix-layout', component: WebixLayoutComponent },
-  { path: 'data-loading', component: DataLoadingComponent },
-  { path: 'html-layout', component: HTMLLayoutComponent },
-  { path: 'form-grid', component: FormToolbarComponent },
-  { path: 'template-routing', component: TemplateRoutingComponent, children: [
-    { path: 'area1', component: WebixLayoutComponent, outlet:"sub" },
-    { path: 'area2',  component: HTMLLayoutComponent, outlet:"sub" }
-  ]},
+  // { path: 'webix-layout', component: WebixLayoutComponent },
+  // { path: 'data-loading', component: DataLoadingComponent },
+  // { path: 'html-layout', component: HTMLLayoutComponent },
+  // { path: 'form-grid', component: FormToolbarComponent },
+  // { path: 'template-routing', component: TemplateRoutingComponent, children: [
+  //   { path: 'area1', component: WebixLayoutComponent, outlet:"sub" },
+  //   { path: 'area2',  component: HTMLLayoutComponent, outlet:"sub" }
+  // ]},
   { path: '', component: HTMLLayoutComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,32 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FormToolbarComponent }   from './components/form-toolbar.component';
-import { HTMLLayoutComponent }   from './components/html-layout.component';
-import { WebixLayoutComponent }   from './components/webix-layout.component';
-import { TemplateRoutingComponent } from './components/template-routing.component';
-import { DataLoadingComponent } from './components/data-loading.component';
-import { SideBarComponent } from './components/sidebar.component';
-import { DataTableComponent } from './components/datatable.component';
-import { ToolbarComponent } from './components/toolbar.component';
-import { ColumnsComponent, RowsComponent, CellComponent } from './components/layout.component';
+// import { FormToolbarComponent }   from './components/form-toolbar.component';
+ import { HTMLLayoutComponent }   from './components/html-layout.component';
+// import { WebixLayoutComponent }   from './components/webix-layout.component';
+// import { TemplateRoutingComponent } from './components/template-routing.component';
+// import { DataLoadingComponent } from './components/data-loading.component';
+// import { SideBarComponent } from './components/sidebar.component';
+ import { DataTableComponent } from './components/datatable.component';
+// import { ToolbarComponent } from './components/toolbar.component';
+// import { ColumnsComponent, RowsComponent, CellComponent } from './components/layout.component';
     
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormToolbarComponent,
+  //  FormToolbarComponent,
     HTMLLayoutComponent,
-    WebixLayoutComponent,
-    TemplateRoutingComponent,
-    DataLoadingComponent,
+   // WebixLayoutComponent,
+    //TemplateRoutingComponent,
+    //DataLoadingComponent,
 
-    DataTableComponent,
-    ToolbarComponent,
-    SideBarComponent,
-    ColumnsComponent,
-    RowsComponent,
-    CellComponent
+  DataTableComponent,
+    //ToolbarComponent,
+    //SideBarComponent,
+    //ColumnsComponent,
+    //RowsComponent,
+    //CellComponent
   ],
   imports: [
     BrowserModule,
